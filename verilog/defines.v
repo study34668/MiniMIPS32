8e57a736a110dae042a35b2374b7f2c17eeb105f
study34668 <2551730844@qq.com>
2020-12-20 12:45:19 +0800

    logisim instructions

--- verilog/defines.v
+++ verilog/defines.v
@@ -51,31 +51,73 @@
 
 // 内部操作码aluop
 `define MINIMIPS32_LUI             8'h05
+
 `define MINIMIPS32_MFHI            8'h0C
 `define MINIMIPS32_MFLO            8'h0D
+`define MINIMIPS32_MTHI            8'h0E
+`define MINIMIPS32_MTLO            8'h0F
+
 `define MINIMIPS32_SLL             8'h11
+`define MINIMIPS32_SRL             8'h12
+`define MINIMIPS32_SRA             8'h13
+
 `define MINIMIPS32_MULT            8'h14
+`define MINIMIPS32_MULTU           8'h15
+
 `define MINIMIPS32_DIV             8'h16
+`define MINIMIPS32_DIVU            8'h17
+
 `define MINIMIPS32_ADD             8'h18
 `define MINIMIPS32_ADDIU           8'h19
 `define MINIMIPS32_SUBU            8'h1B
 `define MINIMIPS32_AND             8'h1C
 `define MINIMIPS32_ORI             8'h1D
+`define MINIMIPS32_XOR             8'h1E
+`define MINIMIPS32_NOR             8'h1F
+
 `define MINIMIPS32_SLT             8'h26
 `define MINIMIPS32_SLTIU           8'h27
+`define MINIMIPS32_SLTI            8'h28
+`define MINIMIPS32_SLTU            8'h29
+
 `define MINIMIPS32_J               8'h2C
 `define MINIMIPS32_JR              8'h2D
 `define MINIMIPS32_JAL             8'h2E
+`define MINIMIPS32_JALR            8'h2F
+
 `define MINIMIPS32_BEQ             8'h30
 `define MINIMIPS32_BNE             8'h31
+`define MINIMIPS32_BLEZ            8'h32
+`define MINIMIPS32_BGTZ            8'h33
+`define MINIMIPS32_BLTZ            8'h34
+`define MINIMIPS32_BGEZ            8'h35
+`define MINIMIPS32_BLTZAL          8'h36
+`define MINIMIPS32_BGEZAL          8'h37
+
+`define MINIMIPS32_SLLV            8'h41
+`define MINIMIPS32_SRLV            8'h42
+`define MINIMIPS32_SRAV            8'h43
+
+`define MINIMIPS32_ADDI            8'h48
+`define MINIMIPS32_ADDU            8'h49
+`define MINIMIPS32_SUB             8'h4B
+`define MINIMIPS32_ANDI            8'h4C
+`define MINIMIPS32_OR              8'h4D
+`define MINIMIPS32_XORI            8'h4E
+
 `define MINIMIPS32_SYSCALL         8'h86
 `define MINIMIPS32_ERET            8'h87
 `define MINIMIPS32_MFC0            8'h8C
 `define MINIMIPS32_MTC0            8'h8D
+
 `define MINIMIPS32_LB              8'h90
+`define MINIMIPS32_LH              8'h91
 `define MINIMIPS32_LW              8'h92
 `define MINIMIPS32_SB              8'h98
+`define MINIMIPS32_SH              8'h99
 `define MINIMIPS32_SW              8'h9A
+`define MINIMIPS32_LBU             8'h9B
+`define MINIMIPS32_LHU             8'h9C
 
 /*------------------- 通用寄存器堆参数 -------------------*/
 `define REG_BUS         31: 0               // 寄存器数据宽度
